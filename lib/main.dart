d33ac8debbf28ee8bc66b628ed09cd790f0b7ea2
Muhammet <72871376+muhammetaydinn@users.noreply.github.com>
2022-03-21 18:39:17 +0300

    a

--- lib/main.dart
+++ lib/main.dart
@@ -51,12 +51,15 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   late FirebaseAuth auth;
+  final String _email = "maydin@gmail.com";
+  final String _password = "zortinger";
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    auth=FirebaseAuth.instance;
+    auth = FirebaseAuth.instance;
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -72,7 +75,14 @@ class _MyHomePageState extends State<MyHomePage> {
                 createUserEmailAndPassword();
               },
               style: ElevatedButton.styleFrom(primary: Colors.red),
-              child: Text("Email/Sifre Giris"),
+              child: Text("Email/Sifre kayıt"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                loginUserEmailAndPassword();
+              },
+              style: ElevatedButton.styleFrom(primary: Colors.red),
+              child: Text("Email/Sifre giris"),
             ),
           ],
         ),
@@ -81,6 +91,22 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   Future<void> createUserEmailAndPassword() async {
-    await auth.createUserWithEmailAndPassword(email: email, password: password)
+    try {
+      var _userCredential = await auth.createUserWithEmailAndPassword(
+          email: _email, password: _password);
+      debugPrint(_userCredential.toString());
+    } catch (e) {
+      debugPrint(e.toString());
+    }
+  }
+
+  void loginUserEmailAndPassword() async {
+    try {
+      var _userCredential = await auth.signInWithEmailAndPassword(
+          email: _email, password: _password);
+      debugPrint(_userCredential.toString());
+    } catch (e) {
+      debugPrint(e.toString());
+    }
   }
 }
