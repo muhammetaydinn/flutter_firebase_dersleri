334eb36fc48a52043365c971924a9f09eb2667cf
Muhammet <72871376+muhammetaydinn@users.noreply.github.com>
2022-03-21 18:02:44 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/aydin/flutter_firebase_dersleri/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.aydin.flutter_firebase_dersleri
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,86 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late FirebaseAuth auth;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    auth = FirebaseAuth.instance;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late FirebaseAuth auth;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    auth=FirebaseAuth.instance;
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+              onPressed: () {
+                createUserEmailAndPassword();
+              },
+              style: ElevatedButton.styleFrom(primary: Colors.red),
+              child: Text("Email/Sifre Giris"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future<void> createUserEmailAndPassword() async {
+    await auth.createUserWithEmailAndPassword(email: email, password: password)
+  }
+}
